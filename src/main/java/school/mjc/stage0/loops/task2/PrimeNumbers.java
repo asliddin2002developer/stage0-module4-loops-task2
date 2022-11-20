e9e6d37a9f6b8b9e41cd49fb6de5bdff5deded87
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-20 09:38:51 +0500

    loops

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        while (counter <= printToInclusive){
+            if (isPrime(counter)){
+                System.out.println(counter);
+            }
+            counter++;
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        int count = 1;
+        for (int i = 2; i <= num;){
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        return count == 2;
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        int length = chars.length;
+        int counter = 0;
+        String res = "";
+        while (counter < length){
+            res += chars[counter];
+            counter++;
+        }
+        System.out.println(res);
     }
 }
